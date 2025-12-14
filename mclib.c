59cdc3386a3909128a15df03d5a27279de27e1c0
Cristiano Camacho <originalvgo@gmail.com>
2025-12-14 15:34:40 -0300

    Translate comments and variable descriptions to English
    Updated comments and variable descriptions to English for better clarity.

--- mclib.c
+++ mclib.c
@@ -1,57 +1,51 @@
-/*   Funcoes da biblioteca interna   */
-
-/* adcione mais aqui, ou seja, coloque aqui sua conta em risco  */
-
-//#include<conio.h>       /* Elimine essa se seu compilador nao suporta esse arquivo de cabecalho */
+/*   Internal library functions   */
+/* add more here, or in other words, put your account at risk  */
+//#include<conio.h>       /* Remove this if your compiler doesn't support this header file */
 #include<stdio.h>
 #include<stdlib.h>
 
-extern char *prog;      /* aponta para a posicao corrente do programa */
+extern char *prog;      /* points to the current program position */
+extern char token[80];  /* holds the string representation of the token  */
+extern char token_type; /* contains the token type */
+extern char tok;        /* holds the internal representation of the token */
 
-extern char token[80];  /* mantem a representacao string do token  */
+enum tok_types {DELIMITER, IDENTIFIER, NUMBER, KEYWORD, TEMP, STRING, BLOCK};
 
-extern char token_type;      /* contem o tipo do token */
-extern char tok;        /* mantem a representacao interna do token */
-
-enum tok_types {DELIMETER, IDENTIFIER, NUMBER, KEYWORD, TEMP, STRING, BLOCK};
-
-/* Essas sao as constantes usadas para chamar sntx_err() quando ocorre um erro de sintaxe. Adicione mais se desejar.
-*  SINTAX eh uma mensagem generica de erro usada quando nenhuma outra parece apropriada.*/
-
+/* These are the constants used to call sntx_err() when a syntax error occurs. Add more if desired.
+*  SYNTAX is a generic error message used when no other seems appropriate.*/
 enum error_msg
 {
     SYNTAX, UNBAL_PARENS, NO_EXP, EQUALS_EXPECTED,
     NOT_VAR, PARAM_ERR, SEMI_EXPECTED,
     UNBAL_BRACES, FUNC_UNDEF, TYPE_EXPECTED,
     NEST_FUNC, RET_NOCALL, PAREN_EXPECTED,
-    WHILE_EXPECTED, QUOTE_EXPECTED, NOTE_STRING, TOO_MANY_LVARS
+    WHILE_EXPECTED, QUOTE_EXPECTED, NOT_STRING, TOO_MANY_LVARS
 };
 
 int get_token(void);
 void sntx_err(int error), eval_exp(int *result);
 void putback(void);
 
-/* Obtem um caractere de console, use getchar() se seu compilador nao suportar getche() */
+/* Gets a character from console, use getchar() if your compiler doesn't support getche() */
 int call_getche()
 {
     char ch;
     ch = getchar();
     while (*prog!=')') prog++;
-    prog++;     /* avanca ate o fim da linha */
+    prog++;     /* advances to the end of the line */
     return ch;
 }
 
-/* exibe um caractere na tela */
+/* displays a character on the screen */
 int call_putch()
 {
     int value;
-
     eval_exp(&value);
     printf("%c", value);
     return value;
 }
 
-/* chama puts */
+/* calls puts */
 int call_puts(void)
 {
     get_token();
@@ -61,67 +55,41 @@ int call_puts(void)
     puts(token);
     get_token();
     if(*token!=')') sntx_err(PAREN_EXPECTED);
-
     get_token();
     if(*token!=';') sntx_err(SEMI_EXPECTED);
     putback();
     return 0;
 }
 
-/* uma funcao embutida de saida para o controle */
+/* a built-in output function for control */
 int print(void)
 {
     int i;
-
     get_token();
     if(*token!='(') sntx_err(PAREN_EXPECTED);
-
     get_token();
-    if(token_type==STRING) {    /* exibe uma string */
+    if(token_type==STRING) {    /* displays a string */
         printf("%s ", token);
     }
-     else {    /* um numero */
+     else {    /* a number */
         putback();
         eval_exp(&i);
         printf("%d ", i);
     }
-
     get_token();
-
     if(*token!=')') sntx_err(PAREN_EXPECTED);
-
     get_token();
     if(*token!=';') sntx_err(SEMI_EXPECTED);
     putback();
     return 0;
 }
 
-/* Le um int do teclado */
+/* Reads an int from the keyboard */
 int getnum(void)
 {
     char s[80];
-
     gets(s);
     while(*prog!=')') prog++;
-    prog++;   /* avanca ate o fim da linha */
+    prog++;   /* advances to the end of the line */
     return atoi(s);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
